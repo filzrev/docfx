f5e2029d229cb6266cd35ada78914ef38f13e8e5
filzrev <103790468+filzrev@users.noreply.github.com>
2024-04-02 09:16:19 +0900

    chore: add test for command help content

---
+++ test/docfx.Tests/CommandLineHelpContractTest.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text;
+using Docfx.Common;
+using Docfx.Tests.Common;
+using FluentAssertions;
+
+namespace Docfx.Tests;
+
+[Collection("docfx STA")]
+public class CommandLineHelpContractTest
+{
+    /// <summary>
+    /// Verify docfx help contents.
+    /// </summary>
+    [Fact]
+    public static Task CommandLineHelpOutputTest()
+    {
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        try
+        {
+            // Print help contents to writer
+            RunHelpCommand();
+            RunHelpCommand("init");
+            RunHelpCommand("build");
+            RunHelpCommand("metadata");
+            RunHelpCommand("serve");
+            RunHelpCommand("pdf");
+            RunHelpCommand("template");
+            RunHelpCommand("download");
+            RunHelpCommand("merge");
+
+            var helpText = sw.ToString();
+            return Verify(new Target("txt", helpText)).UseFileName("Help").AutoVerify(includeBuildServer: false);
+        }
+        finally
+        {
+            Console.SetOut(Console.Out);
+        }
+    }
+
+    private static void RunHelpCommand(string command = "")
+    {
+        Console.WriteLine($"> docfx {command} --help");
+        Program.Main([command, "--help"]).Should().Be(0);
+        Console.WriteLine();
+    }
+}
