43f0941f8b45d26bb102cb11146067f473f14ac4
filzrev <103790468+filzrev@users.noreply.github.com>
2025-07-03 18:55:38 +0900

    chore: modify xml comment to markdown behaviors

---
+++ test/Docfx.Dotnet.Tests/XmlCommentTests/XmlCommentExamplesTest.cs
@@ -0,0 +1,92 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using AwesomeAssertions;
+using Xunit;
+
+namespace Docfx.Dotnet.Tests;
+
+public class XmlCommentExamplesTest
+{
+    [Fact]
+    public void Examples()
+    {
+        // Indent of first line should be preserved
+        ValidateExamples(
+            // Input XML
+            """
+            <example>
+            This sample shows how to call the <see cref="M: Docfx.EntityModel.XmlCommentParser.GetExceptions(System.String, Docfx.EntityModel.XmlCommentParserContext)"/> method.
+            <code>
+            class TestClass
+            {
+                static int Main()
+                {
+                    return GetExceptions(null, null).Count();
+                }
+            } 
+            </code>
+            </example>
+
+            <example>
+            This is another example
+            </example>
+
+            <example>
+            Check empty code.
+            <code></code>
+            </example>
+
+            <example>
+            This is an example using source reference.
+            <code source="Example.cs" region="Example"/>
+            </example>
+            """,
+            // Expected Markdown
+            [
+               // Example #1
+               """
+               This sample shows how to call the <see cref="M: Docfx.EntityModel.XmlCommentParser.GetExceptions(System.String, Docfx.EntityModel.XmlCommentParserContext)"></see> method.
+
+               <pre><code class="lang-csharp">class TestClass
+               {
+                   static int Main()
+                   {
+                       return GetExceptions(null, null).Count();
+                   }
+               }</code></pre>
+               """,
+
+               // Example #2
+               """
+               This is another example
+               """,
+
+               // Example #3
+               """
+               Check empty code.
+
+               <pre><code class="lang-csharp"></code></pre>
+               """,
+
+               // Example #4
+               """
+               This is an example using source reference.
+
+               <pre><code class="lang-cs"></code></pre>
+               """,
+            ]);
+    }
+
+    private void ValidateExamples(string input, string[] expected)
+    {
+        // Act
+        var results = XmlComment.Parse(input).Examples;
+
+        // Assert
+        results.Should().NotBeNull(); // Failed to get summary from XML input.
+
+        results.Should()
+               .BeEquivalentTo(expected, x => x.WithStrictOrdering());
+    }
+}
