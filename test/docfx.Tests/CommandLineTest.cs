0463a4ea814bd90c9b4b1fcded2b5006d4a92880
filzrev <103790468+filzrev@users.noreply.github.com>
2026-09-21 14:54:48 +0900

    t

--- test/docfx.Tests/CommandLineTest.cs
+++ test/docfx.Tests/CommandLineTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
+using Spectre.Console;
 
 namespace Docfx.Tests;
 
@@ -11,15 +12,25 @@ public class CommandLineTest
     [Fact]
     public static void PrintsHelp()
     {
-        //Assert.Equal(0, Program.Main(["-h"]));
-        //Assert.Equal(0, Program.Main(["--help"]));
-        //Assert.Equal(0, Program.Main(["build", "--help"]));
-        //Assert.Equal(0, Program.Main(["serve", "--help"]));
-        //Assert.Equal(0, Program.Main(["metadata", "--help"]));
-        //Assert.Equal(0, Program.Main(["pdf", "--help"]));
-        //Assert.Equal(0, Program.Main(["init", "--help"]));
-        Assert.Equal(0, Program.Main(["download", "--help"]));
-        //Assert.Equal(0, Program.Main(["merge", "--help"]));
-        //Assert.Equal(0, Program.Main(["template", "--help"]));
+        var savedValue = AnsiConsole.Profile.Capabilities.Ansi;
+        AnsiConsole.Profile.Capabilities.Ansi = false;
+        try
+        {
+
+            //Assert.Equal(0, Program.Main(["-h"]));
+            //Assert.Equal(0, Program.Main(["--help"]));
+            //Assert.Equal(0, Program.Main(["build", "--help"]));
+            //Assert.Equal(0, Program.Main(["serve", "--help"]));
+            //Assert.Equal(0, Program.Main(["metadata", "--help"]));
+            //Assert.Equal(0, Program.Main(["pdf", "--help"]));
+            //Assert.Equal(0, Program.Main(["init", "--help"]));
+            Assert.Equal(0, Program.Main(["download", "--help"]));
+            //Assert.Equal(0, Program.Main(["merge", "--help"]));
+            //Assert.Equal(0, Program.Main(["template", "--help"]));
+        }
+        finally
+        {
+            AnsiConsole.Profile.Capabilities.Ansi = savedValue;
+        }
     }
 }
